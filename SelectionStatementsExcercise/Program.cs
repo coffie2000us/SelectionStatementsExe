d6f66ca233c0bdf7215c74875c1c97e013de9186
coffie2000us <charlescoffie.cri@gmail.com>
2021-09-29 12:15:41 -0400

    initial commit

---
+++ SelectionStatementsExcercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SelectionStatementsExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber = 9;
+            Console.WriteLine("Guess favorite number below 10");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favoriteNumber)
+            {
+
+                Console.WriteLine("Not Accurate!!");
+            }
+
+
+            else if (userGuess > favoriteNumber)
+            {
+
+                Console.WriteLine("Too High!!");
+            }
+
+            else if (userGuess == favoriteNumber)
+            {
+                Console.WriteLine("You're Right!!");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
